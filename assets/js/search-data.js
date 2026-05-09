4e26312290b538393100a4ed460b97fdf7fc4a6f
longnk0702 <182729468+longnk0702@users.noreply.github.com>
2026-05-09 01:40:17 +0000

    Deploying to gh-pages from @ longnk0702/longnk0702.github.io@9d8c8b10acb09efcd33fd88a4746b6075851fcb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -549,13 +549,6 @@ ninja.data = [{
           window.open("https://wa.me/84358036759", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
