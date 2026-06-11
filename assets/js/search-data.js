26fc983e035a5a32eed95011a8590593966d81df
longnk0702 <182729468+longnk0702@users.noreply.github.com>
2026-06-11 07:29:50 +0000

    Deploying to gh-pages from @ longnk0702/longnk0702.github.io@dfa6e6b10fde5e59fc076abaf18efe3317c74b53 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -15,13 +15,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-media",
-          title: "media",
-          description: "Selected media articles and news features about my academic journey, research experience, scholarships, and international programs.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/media/";
           },
         },{id: "dropdown-publication",
               title: "publication",
